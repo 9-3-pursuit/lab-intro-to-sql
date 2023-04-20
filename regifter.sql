36afa6653f10369bd608285ecbc7dfaa7f03f660
ariunaamy <ariun.myagmar@gmail.com>
2023-04-19 20:35:03 -0400

    half way

--- regifter.sql
+++ regifter.sql
@@ -16,70 +16,86 @@ CREATE DATABASE regifter;
 -- value - integer
 -- previously_regifted boolean
 
-
+CREATE TABLE gifts (
+    id SERIAL PRIMARY KEY,
+    gift TEXT,
+    giver TEXT,
+    value INT,
+    previously_regifted BOOLEAN
+);
 -- 
 \echo See details of the table you created
 -- 
+\d gifts; 
 
+-- \d gifts
 
--- 
 \echo Alter the table so that the column price is changed to value 
 -- 
-
-
+ALTER TABLE gifts 
+RENAME COLUMN price TO value;
 -- 
 \echo Insert a peach candle, given by 'Santa' thats value is 9 and has been previously regifted
 -- 
-
-
+INSERT INTO                                                                                                                                            
+gifts (gift, giver, price, previously_regifted)                                                                                                                   
+VALUES
+('peach candle', 'Santa', 9, true);
 --
 \echo Query for all the columns in your gifts table
 -- 
-
-
+\echo 'Query for all columns in the gifts table:'
+SELECT column_name
+FROM information_schema.columns
+WHERE table_name = 'gifts';
 --
 \echo Uncomment below to insert 5 more gifts
 -- 
 
--- INSERT INTO gifts (gift, giver, value, previously_regifted)
--- VALUES
--- ('peach candle', 'Santa', '9', TRUE),
--- ('cinnamon candle', 'Nick', '19', TRUE),
--- ('soap on a rope', 'Rudolf', '29', FALSE),
--- ('potpurri', 'Elf on the Shelf', '39', TRUE),
--- ('mango candle', 'The Boss', '49', FALSE)
--- ;
+INSERT INTO gifts (gift, giver, value, previously_regifted)
+VALUES
+('peach candle', 'Santa', '9', TRUE),
+('cinnamon candle', 'Nick', '19', TRUE),
+('soap on a rope', 'Rudolf', '29', FALSE),
+('potpurri', 'Elf on the Shelf', '39', TRUE),
+('mango candle', 'The Boss', '49', FALSE)
+;
 
 -- 
-\echo Insert 5 more gifts of your own choosing,  include 1 more candle
+\echo Insert  more gifts of your own choosing,  include 1 more candle
 --
-
-
+INSERT INTO gifts (gift, giver, value, previously_regifted)
+VALUES
+('dress', 'neighbor', '1000', false),
+('fruits', 'mom', '50', false),
+('knives', 'friend', '200', true),
+('gift card', 'cousin', '300', true),
+('shorts', 'sister', '1000', false);
 
 --
 \echo Query for gifts with a price greater than or equal to 20
 --
 
+SELECT gifts FROM gifts WHERE price>=20;
 
 --
 \echo Query for every gift that has the word candle in it, only show the gift column
 --
 
-
+SELECT gift FROM gifts 
+WHERE gift LIKE '%candle%';
 --
 \echo Query for every gift whose giver is Santa OR value is greater than 30
 --
-
-
+SELECT gift FROM gifts 
+WHERE giver='Santa' OR value>30;
 --
 \echo Query for every gift whose giver is NOT Santa
 --
-
-
+SELECT gift FROM gifts WHERE giver !='Santa';
 --
 \echo Update the second gift to have a value of 2999
 -- 
-
 
 --
 \echo Query for the updated item
